641dc8134b2651870e1a048b52b3d530ddfd537f
Vitaliy Fundovoy <nickvi@bk.ru>
2021-12-06 17:43:10 +0300

    RestAssuredClient was extended with getResponseCode and getResponseBodyFieldByPath0

--- src/main/java/com/example/RestAssuredClient.java
+++ src/main/java/com/example/RestAssuredClient.java
@@ -2,13 +2,23 @@ package com.example;
 
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class RestAssuredClient {
     public RequestSpecification getBaseSpec() {
         return new RequestSpecBuilder()
                 .setContentType(ContentType.JSON)
-                .setBaseUri("")
+                .setBaseUri("https://qa-scooter.praktikum-services.ru/")
                 .build();
+    }
+
+    public static int getResponseCode(Response response) {
+        return response.then().extract().statusCode();
+    }
+
+    public static <T> T getResponseBodyFieldByPath(Response response, String fieldPath) {
+        return response.then().extract().path(fieldPath);
+
     }
 }
